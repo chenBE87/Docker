4f4b53375bf4d29e8b6e1ba74d5a394d4e2828b7
chenBE87 <chencules@gmail.com>
2021-11-03 21:48:19 +0200

    add documents

--- dockerfile
+++ dockerfile
@@ -1,10 +1,17 @@
+# fetch debian package
 FROM debian
 
+# copy python script to the package
 COPY . /app
+
+# update packages and install requiered packages
 RUN apt-get -y update
 RUN apt-get install -y python3 python3-pip wget
 RUN apt install -y firefox-esr
+
+# install firefox driver ( for selenium usage )
 RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
 RUN tar -C /usr/local/bin/ -xvf geckodriver-v0.30.0-linux64.tar.gz
+
+# download and install selenium package for Python
 RUN pip install -U selenium
-CMD ["python3", "--version"]
--- screenshot.py
+++ screenshot.py
@@ -1,14 +1,25 @@
 import sys
 import os
+import requests
 from selenium import webdriver
 from time import sleep
 
+# prepare web driver instance
 opts = webdriver.FirefoxOptions()
 opts.add_argument("--headless")
 driver = webdriver.Firefox(options=opts)
-driver.get(sys.argv[1])
-sleep(1)
 
-driver.get_screenshot_as_file("/tmp/output/screenshot.png")
-driver.quit()
-print("end...")
+# check URL. print error message if URL is blank/invalid
+req = requests.get(sys.argv[1])
+if req.status_code == requests.codes['ok']:
+    # get URL vie Selenium
+    driver.get(sys.argv[1])
+    sleep(1)
+    # take screenshot and save in specific location ( for Docker usage )
+    driver.get_screenshot_as_file("/tmp/output/screenshot.png")
+    # exit driver
+    driver.quit()
+    print("end...")
+else:
+    # page is invalid
+    print('invalid web page!!!!')
